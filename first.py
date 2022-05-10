6d00236188b5b1e028b508ca3094aeb42b57c58c
t0xx3 <77791939+t0xx3@users.noreply.github.com>
2022-05-11 02:01:10 +0300

    first.py
    jst first, k?

---
+++ first.py
@@ -0,0 +1,13 @@
+#Hello Friend
+def main():
+    name = input('Enter ur name: ')
+    dec = ('==='*10)
+    site = ('git.com/t0xx3')
+    print(dec)
+    print(f'Hello {name}')
+    if name == 'XXX':
+        print(f'How are u{name}??? See my site: \n    {site}')
+    else:
+        print(f'Well done, {name} \n    {site}')
+    print(dec)
+main()
